d240115fd5bf9ad0966e4a7bc8dcaefa1282b1dd
Nico May <nico.may99@gmail.com>
2016-02-13 18:14:46 -0800

    options

--- assorted/tests/typwrit/typwrit.js
+++ assorted/tests/typwrit/typwrit.js
@@ -1,4 +1,10 @@
 (function (console) { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
 var HxOverrides = function() { };
 HxOverrides.substr = function(s,pos,len) {
 	if(pos != null && pos != 0 && len != null && len < 0) return "";
@@ -14,7 +20,9 @@ Std["int"] = function(x) {
 	return x | 0;
 };
 var Typwrit = function() {
-	this.text = "\r\nTest.\r\n\r\nSo this appears to be working?\r\n\r\nDo you hear me now?\r\n\r\nBye.\r\n";
+	this.convo = { id : "first", prompt : "\r\n\t\t*cough*\r\n\r\n\t\t*ahem*\r\n\r\n\t\tIs this thing on?\r\n\r\n\t\t*tap* *tap*\r\n\t\t", responses : [{ response : "Seems like it.", prompt : "Oh, uh, ok.", responses : [{ response : "uh, ok, and?", switchTo : "first"}]}]};
+	this.convoIDs = new haxe_ds_StringMap();
+	this.options = [];
 	window.onload = $bind(this,this.ready);
 };
 Typwrit.main = function() {
@@ -23,19 +31,100 @@ Typwrit.main = function() {
 Typwrit.prototype = {
 	ready: function() {
 		this.page = window.document.getElementById("page");
-		this.typeLetters(this.text);
+		this.options[0] = window.document.getElementById("op1");
+		this.options[1] = window.document.getElementById("op2");
+		this.resetTypewriter();
+		this.parseConvoIDs(this.convo);
+		this.playConvo(this.convo);
 	}
-	,typeLetters: function(message) {
-		if(message.length == 0) return;
-		this.page.innerHTML += message.charAt(0);
-		message = HxOverrides.substr(message,1,null);
-		haxe_Timer.delay((function(f,a1) {
+	,parseConvoIDs: function(convo) {
+		if(convo.id != null) {
+			this.convoIDs.set(convo.id,convo);
+			convo;
+		}
+		if(convo.responses != null) {
+			var _g = 0;
+			var _g1 = convo.responses;
+			while(_g < _g1.length) {
+				var r = _g1[_g];
+				++_g;
+				this.parseConvoIDs(r);
+			}
+		}
+	}
+	,playConvo: function(convo) {
+		var _g2 = this;
+		this.resetTypewriter();
+		var curnode = convo;
+		if(curnode.switchTo != null) {
+			if(this.convoIDs.get(curnode.switchTo) != null) {
+				this.playConvo(this.convoIDs.get(curnode.switchTo));
+				return;
+			} else throw new js__$Boot_HaxeError("convoid: " + "'" + curnode.switchTo + "'" + " not defined.");
+		}
+		if(curnode.responses != null) {
+			var _g1 = 0;
+			var _g = curnode.responses.length;
+			while(_g1 < _g) {
+				var i = [_g1++];
+				this.options[i[0]].onclick = (function(i) {
+					return function() {
+						_g2.playConvo(curnode.responses[i[0]]);
+						return;
+					};
+				})(i);
+				this.options[i[0]].style.display = "initial";
+			}
+			if(this.options.length > curnode.responses.length) {
+				var _g11 = curnode.responses.length;
+				var _g3 = this.options.length;
+				while(_g11 < _g3) {
+					var i1 = _g11++;
+					this.options[i1].style.display = "none";
+				}
+			}
+		}
+		if(curnode.prompt != null) {
+			console.log(curnode.prompt);
+			this.typeLetters(curnode.prompt,this.page,function() {
+				_g2.playResponses(curnode);
+			});
+		}
+	}
+	,playResponses: function(convo,index) {
+		if(index == null) index = 0;
+		if(index >= convo.responses.length) return;
+		this.typeLetters(convo.responses[index].response,this.options[index],(function(f,a1,a2) {
 			return function() {
-				f(a1);
+				f(a1,a2);
 			};
-		})($bind(this,this.typeLetters),message),Std["int"](50 + Math.random() * 20));
+		})($bind(this,this.playResponses),convo,++index));
+	}
+	,typeLetters: function(message,element,onComplete) {
+		if(message.length == 0) {
+			onComplete();
+			return;
+		}
+		element.innerHTML += message.charAt(0);
+		message = HxOverrides.substr(message,1,null);
+		haxe_Timer.delay((function(f,a1,a2,a3) {
+			return function() {
+				f(a1,a2,a3);
+			};
+		})($bind(this,this.typeLetters),message,element,onComplete),Std["int"](50 + Math.random() * 20));
+	}
+	,resetTypewriter: function() {
+		this.page.innerHTML = "";
+		var _g = 0;
+		var _g1 = this.options;
+		while(_g < _g1.length) {
+			var o = _g1[_g];
+			++_g;
+			o.innerHTML = "";
+		}
 	}
 };
+var haxe_IMap = function() { };
 var haxe_Timer = function(time_ms) {
 	var me = this;
 	this.id = setInterval(function() {
@@ -59,7 +148,37 @@ haxe_Timer.prototype = {
 	,run: function() {
 	}
 };
+var haxe_ds_StringMap = function() {
+	this.h = { };
+};
+haxe_ds_StringMap.__interfaces__ = [haxe_IMap];
+haxe_ds_StringMap.prototype = {
+	set: function(key,value) {
+		if(__map_reserved[key] != null) this.setReserved(key,value); else this.h[key] = value;
+	}
+	,get: function(key) {
+		if(__map_reserved[key] != null) return this.getReserved(key);
+		return this.h[key];
+	}
+	,setReserved: function(key,value) {
+		if(this.rh == null) this.rh = { };
+		this.rh["$" + key] = value;
+	}
+	,getReserved: function(key) {
+		if(this.rh == null) return null; else return this.rh["$" + key];
+	}
+};
+var js__$Boot_HaxeError = function(val) {
+	Error.call(this);
+	this.val = val;
+	this.message = String(val);
+	if(Error.captureStackTrace) Error.captureStackTrace(this,js__$Boot_HaxeError);
+};
+js__$Boot_HaxeError.__super__ = Error;
+js__$Boot_HaxeError.prototype = $extend(Error.prototype,{
+});
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+var __map_reserved = {}
 Typwrit.main();
 })(typeof console != "undefined" ? console : {log:function(){}});
