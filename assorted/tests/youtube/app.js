a5423af2117bfde4ecfc0720d89d3aa40db19537
Nico May <nico.may99@gmail.com>
2016-09-25 16:31:43 -0700

    remove all task related stuff

--- assorted/tests/youtube/app.js
+++ assorted/tests/youtube/app.js
@@ -4,18 +4,13 @@
         var parts = value.split("; " + name + "=");
         if (parts.length == 2) return parts.pop().split(";").shift();
     }
-    angular.module('app', ['ngRoute'])
+    angular.module('app', ['ngRoute', 'ngAnimate'])
     .config(function($routeProvider, $sceDelegateProvider) {
 		$sceDelegateProvider.resourceUrlWhitelist([
   			'self',
   			'https://www.youtube.com/**'
 		]);
         $routeProvider.when('/', {
-            templateUrl: 'login.html',
-            controller: 'LoginController',
-            controllerAs: 'login'
-        })
-        .when('/tasks', {
             templateUrl: 'main.html'
         })
     })
@@ -241,9 +236,6 @@
 					$rootScope.$broadcast('videoselected', self.playlist);
 				};
 
-				/*googleAuth.renderButton('googlelogin').then(function() {
-					$location.path('/tasks');
-				});*/
 				youtube.init().then(function() {
 					$scope.$watch(function() {return self.search}, function() {
 						self.searchVideos(self.search);
@@ -256,82 +248,4 @@
 			controllerAs: 'videosearch'
 		}
 	})
-    .controller('LoginController', function(){})
-    .controller('TaskController', function($scope, googleAuth, myJson) {
-        var self = this;
-        self.tasks = [];
-
-		if(googleAuth.isSignedIn()) {
-			alert(googleAuth.getUser());
-		}
-
-        self.addTask = function(taskTitle) {
-            self.tasks.push({
-                name: taskTitle,
-                checked: false
-            });
-            self.saveTasks();
-        };
-
-        self.removeTask = function(task) {
-            self.tasks.splice(self.tasks.indexOf(task), 1);
-            self.saveTasks();
-        };
-
-        self.saveTasks = function() {
-            var tasksJSON = JSON.stringify(self.tasks);
-			myJson.updateData('4lj7c', tasksJSON).then(function(d) {
-			}, function(e) {
-				console.log(e);
-			});
-            //document.cookie = 'tasks='+tasksJSON+'; expires=Fri, 3 Aug, 2035, 20:47:11 UTC; path=/';
-        }
-
-        self.loadTasks = function() {
-            //var tasksJSON = getCookie('tasks');
-			myJson.retrieveData('4lj7c').then(function(d) {
-				console.log(d);
-				self.tasks = d.data;
-			}, function(e) {
-				alert(e.status);
-			});
-            /*if(tasksJSON != undefined) {
-                self.tasks = JSON.parse(tasksJSON);
-            }*/
-        }
-
-        self.loadTasks();
-
-		setInterval(self.loadTasks, 1000);
-    })
-    .directive('tasklist', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            templateUrl: 'tasklist.html',
-            controller: 'TaskController as tasks'
-        };
-    })
-    .directive('task', function() {
-        return {
-            restrict: 'E',
-            transclude: 'true',
-            require: '^tasklist',
-            templateUrl: 'task.html'
-        };
-    })
-    /*.directive('googlelogin', function() {
-        return {
-            restrict: 'E',
-            template: '<a class="g-signin2" data-onsuccess="console.log" data-theme="dark"></a>',
-            link: function(scope, element) {
-                var script = angular.element('<script/>');
-                script.attr({
-                    src: 'https://apis.google.com/js/platform.js?onload=login.renderButton',
-                    type: 'text/javascript'
-                });
-                element.append(script);
-            }
-        };
-    })*/
 })(window);
